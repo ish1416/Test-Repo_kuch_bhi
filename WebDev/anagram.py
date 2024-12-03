f7e51cb00937e494d053bf2fffdee1f333f40e29
Tushar Verma <tusharmyself06@gmail.com>
2024-12-03 11:32:30 +0530

    hey first commit

---
+++ WebDev/anagram.py
@@ -0,0 +1,13 @@
+s=input()
+lower=0
+upper=0
+for i in s:
+    if i.islower():
+        lower+=1
+    else:
+        upper+=1  
+if lower >= upper:
+    print(s.lower())
+else: 
+    print(s.upper())
+    
